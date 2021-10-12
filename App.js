d4cea99935151528147844d74fcc9ca81e903e4a
PraneethVithanage <37531109+PraneethVithanage@users.noreply.github.com>
2021-10-12 17:55:41 +0530

    Campus Social media

---
+++ App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { createAppContainer,createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack' 
+import Home from './screens/Home.js';
+import Loging from './screens/Loging';
+import Loading from './screens/Loading';
+import Register from './screens/Register';
+import Notification from './screens/Notification';
+import Posts from './screens/Posts';
+import Profile from './screens/Profile';
+import Message from './screens/Message';
+
+
+//  export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Loging/>
+//     );
+//   }
+// } 
+
+const AppStack = createStackNavigator(
+
+  { Home: Home } 
+)
+
+const AuthStack = createStackNavigator({
+  Loging: Loging,
+  Register: Register,
+  Profile:Profile,
+  Message:Message,
+  Posts:Posts,
+  Notification:Notification
+
+});
+
+
+
+export default createAppContainer (
+  createSwitchNavigator (
+      {
+        Loging: Loging,
+        App : AppStack,
+        Auth: AuthStack
+
+      },
+      {
+        initialRouteName:"Loging"
+      }
+
+  )
+
+);
+
---
+++ screens/Register.js
@@ -0,0 +1,182 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+  LayoutAnimation
+} from "react-native";
+import axios from 'axios';
+
+export default class Register extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    usertype:"",
+    errorMessage: null
+  };
+
+  static navigationOptions = {
+    header: null
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content"></StatusBar>
+
+        <Image
+          source={require("../assets/coverr.jpg")}
+          style={{ position: "absolute", marginTop: -10 }}
+        ></Image>
+
+        <TouchableOpacity>
+          <Image
+            source={require("../assets/propic.jpg")}
+            style={{ marginTop: 30, position: "absolute", alignSelf: "center" }}
+          ></Image>
+        </TouchableOpacity>
+
+        <Text style={styles.greeting}>{"Hello \n Sing Up to get start."}</Text>
+
+        <View style={styles.errorMessage}>
+          <Text style>{"Error"}</Text>
+        </View>
+
+        <View style={styles.form}>
+          <View>
+            <Text style={styles.inputTitle}>Full Name </Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              onChangeText={username => this.setState({ username })}
+              value={this.state.username}
+            ></TextInput>
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <Text style={styles.inputTitle}>User Type </Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              onChangeText={usertype => this.setState({ usertype })}
+              value={this.state.usertype}
+            ></TextInput>
+          </View>
+          <View style={{ marginTop: 10 }}>
+            <Text style={styles.inputTitle}>Email Address </Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email}
+            ></TextInput>
+          </View>
+
+          <View style={{ marginTop: 10 }}>
+            <Text style={styles.inputTitle}>Password</Text>
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              autoCapitalize="none"
+              onChangeText={password => this.setState({ password })}
+              value={this.state.password}
+            ></TextInput>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.Register}
+        >
+          <Text style={{ color: "#FFF", fontWeight: "500" }}>Sing Up</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{ alignSelf: "center", marginTop: 32 }}
+          onPress={() => this.props.navigation.navigate("Loging")}
+        >
+          <Text style={{ color: "#414959", fontSize: 13 }}>
+            {" "}
+            New to Social App ?
+            <Text style={{ color: "#E9446A", fontWeight: "500" }}> Login </Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  constructor(props) {
+    super(props);
+    this.state = { username:"",email:"",password:"",usertype:""};
+  }
+  Register= async () => {
+  
+    try {
+      const res = await axios.post("http://192.168.8.100:5000/api/v1/user", {
+        username: this.state.username,
+        email: this.state.email,
+        password: this.state.password,
+        usertype: this.state.usertype
+      });
+      this.props.navigation.navigate("Loging")
+    } catch (err) {
+      console.log(err.message);
+      alert(err.message);
+    }
+  };
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  greeting: {
+    marginTop: 172,
+    fontSize: 18,
+    fontWeight: "400",
+    textAlign: "center"
+  },
+  errorMessage: {
+    height: 72,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 30
+  },
+  error: {
+    color: "#E9446A",
+    fontSize: 13,
+    fontWeight: "600",
+    textAlign: "center"
+  },
+  form: {
+    marginBottom: 18,
+    marginHorizontal: 30
+  },
+
+  inputTitle: {
+    color: "#8A8F9E",
+    fontSize: 10,
+    textTransform: "uppercase"
+  },
+  input: {
+    borderBottomColor: "#8A8F9E",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    height: 40,
+    fontSize: 15,
+    color: "#161F3D"
+  },
+  button: {
+    marginHorizontal: 30,
+    backgroundColor: "#E9446A",
+    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 30
+  }
+});
